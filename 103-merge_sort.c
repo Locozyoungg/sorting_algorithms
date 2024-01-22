27ad8f27105122c6446290fbd69feb8138663e4d
Locozyoungg <collaustin27@gmail.com>
2024-01-22 13:33:01 -0800

    103fix

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,89 +1,106 @@
-/* File: 103-merge_sort.c */
-#include <stddef.h>
+#include "sort.h"
 #include <stdlib.h>
-#include <unistd.h>
-
-void print_array(const int *array, size_t size);
-void print_number(int n);
+#include <stdio.h>
 
 /**
- * merge - Merges two subarrays of array[]
- * @array: The array to be sorted
- * @left: Starting index of the left subarray
- * @mid: Middle index
- * @right: Ending index of the right subarray
+ * merge - Merges two subarrays of 'array'.
+ * @array: The array to be sorted.
+ * @l: Left index of the subarray.
+ * @m: Middle index of the subarray.
+ * @r: Right index of the subarray.
  */
-void merge(int *array, size_t left, size_t mid, size_t right)
+static void merge(int *array, size_t l, size_t m, size_t r)
 {
-	size_t i, j, k;
-	size_t n1 = mid - left + 1;
-	size_t n2 = right - mid;
+    size_t i, j, k;
+    size_t n1 = m - l + 1;
+    size_t n2 = r - m;
 
-	int *left_array = malloc(n1 * sizeof(int));
-	int *right_array = malloc(n2 * sizeof(int));
+    int *L = malloc(n1 * sizeof(int));
+    int *R = malloc(n2 * sizeof(int));
 
-	if (!left_array || !right_array)
-	{
-		free(left_array);
-		free(right_array);
-		return;
-	}
-	for (i = 0; i < n1; i++)
-		left_array[i] = array[left + i];
+    if (!L || !R)
+    {
+        free(L);
+        free(R);
+        return;
+    }
 
-	for (j = 0; j < n2; j++)
-		right_array[j] = array[mid + 1 + j];
-	i = 0;
-	j = 0;
-	k = left;
+    for (i = 0; i < n1; i++)
+        L[i] = array[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = array[m + 1 + j];
 
-	while (i < n1 && j < n2)
-	{
-		if (left_array[i] <= right_array[j])
-		{
-			array[k] = left_array[i];
-			i++;
-		}
-		else
-		{
-			array[k] = right_array[j];
-			j++;
-		}
-		k++;
-	}
-	while (i < n1)
-	{
-		array[k] = left_array[i];
-		i++;
-		k++;
-	}
-	while (j < n2)
-	{
-		array[k] = right_array[j];
-		j++;
-		k++;
-	}
-	free(left_array);
-	free(right_array);
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            array[k] = L[i];
+            i++;
+        }
+        else
+        {
+            array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        array[k] = R[j];
+        j++;
+        k++;
+    }
+
+    free(L);
+    free(R);
 }
 
 /**
  * merge_sort - Sorts an array of integers in ascending order using the
- *              Merge sort algorithm (top-down approach)
- * @array: The array to be sorted
- * @size: Number of elements in the array
+ *              Merge sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
  */
 void merge_sort(int *array, size_t size)
 {
-	if (size > 1)
-	{
-		size_t mid = size / 2;
-		size_t left_size = mid;
-		size_t right_size = size - mid;
+    if (size > 1)
+    {
+        size_t m = size / 2;
+        size_t i;
 
-		merge_sort(array, left_size);
-		merge_sort(array + mid, right_size);
+        int *L = malloc(m * sizeof(int));
+        int *R = malloc((size - m) * sizeof(int));
 
-		merge(array, 0, mid - 1, size - 1);
-	}
+        if (!L || !R)
+        {
+            free(L);
+            free(R);
+            return;
+        }
+
+        for (i = 0; i < m; i++)
+            L[i] = array[i];
+
+        for (i = m; i < size; i++)
+            R[i - m] = array[i];
+
+        merge_sort(L, m);
+        merge_sort(R, size - m);
+        merge(array, 0, m - 1, size - 1);
+
+        free(L);
+        free(R);
+    }
 }
+
